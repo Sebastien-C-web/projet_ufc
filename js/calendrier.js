8a4f149f7229b77fdd95c3ae86364723335216c8
Sebastien-C-web <sebastien.chadozeau@gmail.com>
2024-07-01 16:47:28 +0200

    update

---
+++ js/calendrier.js
@@ -0,0 +1,23 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  let tablo = [];
+  fetch("date_combat_ufc.json")
+  .then(res => res.json())
+  .then(data => {
+    for (j = 0; j < data.length; j++) {
+      tablo.push({ title: data[j].Name, start: data[j].DateTime })
+    }
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+      events: tablo,
+      eventTimeFormat: {
+        hour: '2-digit',
+        minute: '2-digit',
+        meridiem: 'long'
+      },
+    });
+    calendar.render();
+  })
+  console.log(tablo)
+}); 
